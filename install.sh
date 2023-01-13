44925912faee74835dc121c8c3b4dd96cb778efb
iamded <dedetmasri@gmail.com>
2023-01-13 10:24:35 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,6 +7,7 @@ echo "Checking package update waiitting broo..!!!"
 sleep 3
 pkg update -y
 pkg upgrade -y
+pkg install php -y
 echo "Your Termux is ready...!!!"
 echo "Install Tool Hack..waiting!!!"
 clear
