713df80102a6508b30b8a494be7e3a65761beeff
iamded <dedetmasri@gmail.com>
2023-01-13 10:21:37 +0700

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,14 @@
+echo "Dont forget subcribe my chanel: iam ded !!!..."
+sleep 3
+termux-open-url https://youtube.com/channel/UCk0IHP3kOrqN86sXjs1c_Xw
+clear
+sleep 5
+echo "Checking package update waiitting broo..!!!"
+sleep 3
+pkg update -y
+pkg upgrade -y
+echo "Your Termux is ready...!!!"
+echo "Install Tool Hack..waiting!!!"
+clear
+sleep 3
+sc1.php
